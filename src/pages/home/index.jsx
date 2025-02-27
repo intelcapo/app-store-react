18526a56eedffe747a644c0d66acf2ee80a0da1a
William Forero <williamforerob@gmail.com>
2025-02-27 08:52:13 -0500

    consuming fake store API and grid for cards

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,27 @@
+import { useState, useEffect } from "react";
 import Card from "../../components/card";
 function HomePage() {
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		fetch("https://api.escuelajs.co/api/v1/products")
+			.then((productsResponse) => {
+				return productsResponse.json();
+			})
+			.then((data) => {
+				setProducts(data);
+			});
+	}, []);
+
+	console.log(products);
 	return (
 		<>
 			<h1>Home</h1>
-			<Card />
+			<div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+				{products?.map((product) => (
+					<Card key={product.id} product={product} />
+				))}
+			</div>
 		</>
 	);
 }
